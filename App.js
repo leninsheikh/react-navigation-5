a57430ebaa9c936b811995a42755bf6cb396ca77
lenin <justtlenin@gmail.com>
2020-06-14 15:35:53 +0600

    design login page with paper

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AppLoading } from 'expo';
 import Navigator from './navigators/NavigatorContainer';
 import { Provider } from 'react-native-paper';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { enableScreens } from 'react-native-screens';
+import { Container } from 'native-base';
+import LoginScreen from './screens/LoginScreen';
 
 
+enableScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    Roboto: require('native-base/Fonts/Roboto.ttf'),
+    Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    ...Ionicons.font
+  });
+}
 
 export default function App() {
-  return <Provider><Navigator /></Provider> ;
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setFontLoaded(true)}
+    />
+  }
+  // return <Navigator />;
+  return <LoginScreen />
 }
